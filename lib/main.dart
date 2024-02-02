856cd1b0aa48a70955c9c1705e6169d01b134b9c
rstubryan <restubumirr@gmail.com>
2024-02-02 09:56:14 +0700

    Add a task form and the widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mysql_express/screens/add_task.dart';
 import 'package:flutter_mysql_express/screens/home_screen.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const AddTask(),
     );
   }
 }
---
+++ lib/screens/add_task.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mysql_express/utils/app_colors.dart';
+import 'package:flutter_mysql_express/widgets/button_widget.dart';
+import 'package:flutter_mysql_express/widgets/textfield_widget.dart';
+
+class AddTask extends StatelessWidget {
+  const AddTask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController nameController = TextEditingController();
+    TextEditingController detailController = TextEditingController();
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(
+              "assets/addtask1.jpg",
+            ),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                const SizedBox(
+                  height: 40,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: AppColors.secondaryColor,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                TextFieldWidget(
+                  textController: nameController,
+                  hintText: "Task name",
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFieldWidget(
+                  textController: detailController,
+                  hintText: "Task detail",
+                  borderRadius: 15,
+                  maxLines: 3,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ButtonWidget(
+                    backgroundcolor: AppColors.mainColor,
+                    text: "Add",
+                    textColor: Colors.white)
+              ],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 6,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
