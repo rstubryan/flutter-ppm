e13c843071ba823eb39eadc49a038c46c739408b
rstubryan <restubumirr@gmail.com>
2024-02-02 08:46:05 +0700

    Initial commit, create a landing pages

---
+++ android/app/src/main/kotlin/com/example/flutter_mysql_express/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_mysql_express
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mysql_express/utils/app_colors.dart';
+import 'package:flutter_mysql_express/widgets/button_widget.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RichText(
+              text: TextSpan(
+                text: "Hello",
+                style: TextStyle(
+                    color: AppColors.mainColor,
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold),
+                children: [
+                  TextSpan(
+                    text: "\nIt's a good day ain't it?",
+                    style: TextStyle(
+                      color: AppColors.smallTextColor,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 2.5,
+            ),
+            ButtonWidget(
+                backgroundcolor: AppColors.mainColor,
+                text: "Add task",
+                textColor: Colors.white),
+            const SizedBox(
+              height: 20,
+            ),
+            ButtonWidget(
+                backgroundcolor: Colors.white,
+                text: "View All",
+                textColor: AppColors.smallTextColor),
+          ],
+        ),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage("assets/welcome.jpg"),
+          ),
+        ),
+      ),
+    );
+  }
+}
