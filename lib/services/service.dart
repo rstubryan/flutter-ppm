d9b0d966ef04e601b2af17a5ee8180a1d93fb88b
rstubryan <restubumirr@gmail.com>
2024-02-03 11:54:24 +0700

    Fix endpoint API

--- lib/services/service.dart
+++ lib/services/service.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 class DataService extends GetConnect implements GetxService {
   Future<Response> getData() async {
     Response response = await get(
-      "http://http://localhost:3000/tasks/",
+      "http://localhost:3000/tasks/",
       headers: {
         'Content-Type': 'application/json; charset=UTF-8',
       },
